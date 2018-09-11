a47ae1947d1069de3563d369a0d1d0879632acc0
huangdan <787644609@qq.com>
2018-09-11 20:16:06 +0800

    rotate

--- src/pages/rotate.vue
+++ src/pages/rotate.vue
@@ -1,13 +1,37 @@
 <template>
-    $END$
+  <div>
+    <img src="../assets/mm.png" alt="" class="Rotation img">
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "rotate"
-    }
+  // import jquery from 'jquery'
+  export default {
+    name: "rotate"
+  }
+  $(function () {
+    setTimeout(function () {
+      $(".img").addClass("dev");
+      $(".img").hide();
+    },5000)
+  });
 </script>
 
 <style scoped>
-
+  img{
+    width: 40px;
+  }
+  .Rotation {
+    transform: rotate(360deg);
+    animation: rotation 2s linear infinite;
+  }
+  @keyframes rotation {
+    from {-webkit-transform: rotate(0deg);
+    }to {
+       -webkit-transform: rotate(360deg);
+     }
+  }
+  .dev{
+    animation-play-state: paused;
+  }
 </style>
